cefd98b235f1b7be20b4c1add71814ccabb0dc40
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-12-27 22:30:22 +0800

    Added some tailwind configurations, added imgs, and added google font link

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Poppins",
+    },
+    container: {
+      padding: {
+        DEFAULT: "30px",
+        lg: "0px",
+      },
+    },
+    screens: {
+      sm: "760px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: "#222222",
+        secondary: "#F5E6E0",
+      },
+      backgroundImage: {
+        hero: "url('./img/bg_hero.svg')",
+      },
+    },
   },
   plugins: [],
 };
